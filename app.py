f6909cb5934d1aa5d7c5d603be33c2fbbfe9c4f9
B-Turner5 <turnerbenjamin345@gmail.com>
2023-11-06 13:19:44 +0000

    Basic Input return to app through API route

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
+
+@app.route('/process_prompt', methods=['POST'])
+def process_input():
+    data = request.get_json()
+    print(data)
+    prompt = data.get('prompt')
+    
+    return "Input recieved by Flask"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
