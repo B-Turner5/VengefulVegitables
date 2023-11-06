e1bb1af27929f717bd74af0dec5fece6429172f5
B-Turner5 <turnerbenjamin345@gmail.com>
2023-11-06 13:57:56 +0000

    Added prompt generation using stability AI API

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, jsonify, request
+import os
+import io
+import warnings
+from PIL import Image
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+import random
 
 app = Flask(__name__)
+
+os.environ["STABILITY_KEY"] = "sk-iWZM3tgSXtWq08nOkeBIEPx1mbh3W5AIaHqqHwIpZMnH2eXz"
+os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
+
+stability_api = client.StabilityInference(
+    key=os.environ['STABILITY_KEY'],
+    verbose=True,
+    engine="stable-diffusion-xl-1024-v1-0"
+)
+
+
 
 @app.route("/")
 def hello_world():
@@ -9,9 +27,35 @@ def hello_world():
 @app.route('/process_prompt', methods=['POST'])
 def process_input():
     data = request.get_json()
-    print(data)
-    prompt = data.get('prompt')
-    
+    userPrompt = data.get('prompt')
+
+    image = stability_api.generate(
+    prompt=userPrompt + " on a completely white background.",
+    seed=random.randint(1,100000), # If a seed is provided, the resulting generated image will be deterministic.
+                     # What this means is that as long as all generation parameters remain the same, you can always recall the same image simply by generating it again.
+                     # Note: This isn't quite the case for Clip Guided generations, which we'll tackle in a future example notebook.
+    steps=30, # Amount of inference steps performed on image generation. Defaults to 30. 
+    cfg_scale=8.0, # Influences how strongly your generation is guided to match your prompt.
+                   # Setting this value higher increases the strength in which it tries to match your prompt.
+                   # Defaults to 7.0 if not specified.
+    width=1024, # Generation width, defaults to 512 if not included.
+    height=1024, # Generation height, defaults to 512 if not included.
+    samples=1, # Number of images to generate, defaults to 1 if not included.
+    sampler=generation.SAMPLER_K_DPMPP_2M # Choose which sampler we want to denoise our generation with.
+                                                 # Defaults to k_dpmpp_2m if not specified. Clip Guidance only supports ancestral samplers.
+                                                 # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_dpmpp_2s_ancestral, k_lms, k_dpmpp_2m, k_dpmpp_sde)
+)
+    for resp in image:
+        for artifact in resp.artifacts:
+            if artifact.finish_reason == generation.FILTER:
+                warnings.warn(
+                    "Your request activated the API's safety filters and could not be processed."
+                    "Please modify the prompt and try again.")
+            if artifact.type == generation.ARTIFACT_IMAGE:
+                img = Image.open(io.BytesIO(artifact.binary))
+                img.save(str(artifact.seed)+ ".png") # Save our generated images with their seed number as the filename.
+
+
     return "Input recieved by Flask"
 
 
