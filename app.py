85fec1a86c6157f9704cff3642ed5abd3daf0da8
cjhosken <hoskenchristopher@gmail.com>
2023-11-07 19:12:35 +0000

    gen_ai and env fixes

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def artifact_shit(userPrompt):
                     "Please modify the prompt and try again.")
             if artifact.type == generation.ARTIFACT_IMAGE:
                 img = Image.open(io.BytesIO(artifact.binary))
-                img.save("./static/generated/" + str(artifact.seed)+ ".png") # Save our generated images with their seed number as the filename.
+                img.save("static/generated/" + str(artifact.seed)+ ".png") # Save our generated images with their seed number as the filename.
     filename = "static/generated/" + str(artifact.seed) + ".png"
     remove_background(filename)
     return filename
