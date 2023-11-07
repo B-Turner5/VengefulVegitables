9f0b2ceb90f7b9a39c684fa7bf41d777d67eb096
cjhosken <hoskenchristopher@gmail.com>
2023-11-07 14:43:29 +0000

    home page ui

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def artifact_shit(userPrompt):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, ssl_context='adhoc')
+    app.run(host="0.0.0.0", debug=True, ssl_context="adhoc")
 
 
 
