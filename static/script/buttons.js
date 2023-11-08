4364e1dd713edfef1b28e1eafff7bbcdc3fd9441
cjhosken <hoskenchristopher@gmail.com>
2023-11-08 14:37:28 +0000

    camera and new buttons

---
+++ static/script/buttons.js
@@ -0,0 +1,15 @@
+document.getElementById('open-camera-button').addEventListener('click', function () {
+    document.getElementById("camera-overlay").style.display = "flex";
+})
+
+document.getElementById('close-camera-button').addEventListener('click', function () {
+    document.getElementById("camera-overlay").style.display = "none";
+})
+
+document.getElementById('clean-image-button').addEventListener('click', function () {
+    var splashContent = document.querySelector(".splash-content");
+})
+
+document.getElementById('purchase-button').addEventListener('click', function () {
+    var splashContent = document.querySelector(".splash-content");
+})
