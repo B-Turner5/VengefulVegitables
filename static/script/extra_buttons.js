29ba52fd266f5ed281cd081c3ec47a13d1339b21
B-Turner5 <turnerbenjamin345@gmail.com>
2023-11-09 14:10:23 +0000

    Fixed the issue with output images not appearing

--- static/script/extra_buttons.js
+++ static/script/extra_buttons.js
@@ -1,32 +1,47 @@
 document.getElementById('open-camera-button').addEventListener('click', function () {
-    document.getElementById("camera-overlay").style.display = "flex";
+  document.getElementById("camera-overlay").style.display = "flex";
 })
 
 document.getElementById('close-camera-button').addEventListener('click', function () {
-    document.getElementById("camera-overlay").style.display = "none";
+  document.getElementById("camera-overlay").style.display = "none";
 })
 
-document.getElementById('clear-image-button').addEventListener('click', function () {
-    let canvas = document.getElementById("drawing-canvas");
-    let ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawingMade = false;
+let ctx = canvas.getContext("2d");
 
-    document.getElementById('gen-button-container').style.display = "flex";
-    document.getElementById('extra-button-container').style.display = "none";
-    document.getElementById('output-image').style.display = "none";
-    document.getElementById('loader').style.display = "none";
+document.getElementById('clear-image-button').addEventListener('click', function () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawingMade = false;
+
+  document.getElementById('gen-button-container').style.display = "flex";
+  document.getElementById('extra-button-container').style.display = "none";
+  document.getElementById('output-image').style.display = "none";
+  document.getElementById('loader').style.display = "none";
 })
 
 
 
 // THIS FUNCTION DOES NOT SEEM TO RUN. I HAVE NO IDEA WHY
 document.getElementById('purchase-item-button').addEventListener('click', function () {
-    const apiKey = 'OdCOVs1LdtwCLoeC5VE22JHETdNSyq75aY2ahGdt'; 
-    const img = document.getElementById("output-image");
-    context.drawImage(img, canvas.width, canvas.height);
-    const base64_image = canvas.toDataURL();
-    console.log(base64_image)
+  const apiKey = 'OdCOVs1LdtwCLoeC5VE22JHETdNSyq75aY2ahGdt'; 
+  var img = document.getElementById('output-image');
+
+  // Create a canvas element
+  var myCanvas = document.createElement('canvas');
+  var myContext = myCanvas.getContext('2d');
+
+  // Set canvas dimensions to match the image
+  myCanvas.width = img.naturalWidth;
+  myCanvas.height = img.naturalHeight;
+
+  // Draw the image onto the canvas
+  myContext.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
+
+  // Get the base64 data from the canvas
+  var base64String = myCanvas.toDataURL('image/png'); // You can change the format as needed
+
+  // Display or use the base64 string
+  console.log('Base64 String:', base64String);
+
 
     const options = {
         method: 'POST',
@@ -35,7 +50,7 @@ document.getElementById('purchase-item-button').addEventListener('click', functi
           Authorization: `Bearer ${apiKey}`,
         },
         body: JSON.stringify({
-          image_url: base64_image,
+          image_url: base64String,
           item_code: "RNA1",
           name: "Limited Edition Vengeful Vegetables Tee ",
           colours: "White",
